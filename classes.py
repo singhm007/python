78cb01bdf7c7fc2eb8a9c858772d7ed299528dab
singhm007 <king.zomby@gmail.com>
2020-04-05 01:23:59 +0530

    added some code

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 => Python is an object oriented programming language.
 
-=> lmost everything in Python is an object, with its properties and methods.
+=> almost everything in Python is an object, with its properties and methods.
 
 class MyClass:
   x = 5
@@ -62,3 +62,67 @@ p.printall()
 Full name = Manish Singh
 Manish Singh graduation Year is 2005
 
+## Polymorphism / Inheritence
+
+class Animal:
+    def __init__(self,name):
+        self.name = name 
+    def bark(self):
+        raise NotImplementedError("Please implement this method")
+
+class Dog(Animal):
+    def bark(self):
+        print(f"{self.name} says WHOOF !!")
+
+mydog = Dog("Frankie")
+mydog.bark() => Frankie says WHOOF !!
+
+
+
+class Book:
+    def __init__(self,title,author,page):
+        self.title = title
+        self.author = author
+        self.page = page
+    
+    def __str__(self): => Special function  for representing object into string formatt
+        return f"{self.title} is written by {self.author}"
+
+    def __len__(self): => special function for inbuild len represetation for user defined class.
+        return self.page
+
+mybook = Book('Python','Jose',78)
+print(mybook) => Python is written by Jose
+len(mybook) => 78 
+
+=====================================================
+## Simple Programm 
+class Account:
+    
+    def __init__(self,name, balance):
+        self.name = name
+        self.balance = balance
+    
+    def deposit(self,amount):
+        self.balance = self.balance + amount
+        print(f"Total balance : {self.balance}")
+    def withdraw(self,amount):
+        if (self.balance >=  amount):
+            self.balance = self.balance - amount
+            print(f"Total balance : {self.balance}")
+        else:
+            print("Not sufficient balance")
+    def check_balance(self):
+        return self.balance
+    
+        
+    def __str__(self):
+        return f"""Account owner : {self.name}
+Account balance : ${self.balance}
+        """
+
+myaccount = Account('Manish',100)
+myaccount.check_balance()
+myaccount.deposit(200)
+myaccount.withdraw(300)
+print(myaccount)
